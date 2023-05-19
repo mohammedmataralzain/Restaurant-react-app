8273255035d00344528224b60913b030691ea6a5
mohammed matar <mtrm47639@gmail.com>
2023-05-19 19:32:09 +0300

    changes about images file

--- src/constants/images.js
+++ src/constants/images.js
@@ -9,7 +9,6 @@ import knife from '../assets/knife.png';
 import logo from '../assets/logo.png';
 import menu from '../assets/menu.png';
 import overlaybg from '../assets/overlaybg.png';
-// import spoon from '../assets/spoon.svg';
 import welcome from '../assets/welcome.png';
 import findus from '../assets/findus.png';
 // import laurels from '../assets/laurels.png';
@@ -33,7 +32,6 @@ export default {
   logo,
   menu,
   overlaybg,
-  // spoon,
   welcome,
   findus,
   // laurels,
