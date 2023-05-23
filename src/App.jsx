faf962e579deedf99a7305459ca408c3b581b402
mohammed matar <mtrm47639@gmail.com>
2023-05-23 19:11:37 +0300

    changes about app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+
+//Containers
 import Navbar from "./components/Navbar";
 import Header from "./containers/Header"
-import "./app.css"
 import AboutUs from "./containers/AboutUs";
 import SpecialMenu from "./containers/SpecialMenu/SpecialMenu";
 import Chef from "./containers/Chef/Chef";
 
+//style css
+import "./app.css"
 function App() {
 
 
