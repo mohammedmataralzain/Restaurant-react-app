c2fb9be7c28fd44ff0d17bf382bc23c4a7d73f99
mohammed matar <mtrm47639@gmail.com>
2023-06-25 19:17:26 +0300

    add id Subscribe just

--- src/containers/Subscribe/Subscribe.jsx
+++ src/containers/Subscribe/Subscribe.jsx
@@ -3,7 +3,7 @@ import SubHeading from "../../components/SubHeading";
 import "./style.css"
 const Subscribe = () => {
   return (
-    <section className="app__subscribe section__padding">
+    <section className="app__subscribe section__padding" id="Subscribe">
         <SubHeading title="Newsletter" />
         <h3 className="headtext__cormorant" style={{fontSize : "3rem" }}>Subscribe to Our Newsletter</h3>
         <p className="p__open-sans"  style={{margin: "1.5rem 0 3rem 0"}}>And never miss latest Updates!</p>
