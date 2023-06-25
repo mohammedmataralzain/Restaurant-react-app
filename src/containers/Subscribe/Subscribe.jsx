6d84f8b84d5522eb615771d499012909dcd9568f
mohammed matar <mtrm47639@gmail.com>
2023-06-25 18:43:40 +0300

    create Subscribe with style

---
+++ src/containers/Subscribe/Subscribe.jsx
@@ -0,0 +1,18 @@
+import Button from "../../components/button";
+import SubHeading from "../../components/SubHeading";
+import "./style.css"
+const Subscribe = () => {
+  return (
+    <section className="app__subscribe section__padding">
+        <SubHeading title="Newsletter" />
+        <h3 className="headtext__cormorant" style={{fontSize : "3rem" }}>Subscribe to Our Newsletter</h3>
+        <p className="p__open-sans"  style={{margin: "1.5rem 0 3rem 0"}}>And never miss latest Updates!</p>
+        <form>
+            <input type="email" required placeholder="Email Address"/>
+            <Button title="Subscribe"/>
+        </form>
+    </section>
+  )
+}
+
+export default Subscribe
