32c3f7fe958e236103a2c14ae3a47cae7dbf597d
mohammed matar <mtrm47639@gmail.com>
2023-06-25 18:34:55 +0300

    Add changes to app files

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ import Header from "./containers/Header";
 import AboutUs from "./containers/AboutUs";
 import SpecialMenu from "./containers/SpecialMenu/SpecialMenu";
 import Chef from "./containers/Chef/Chef";
+import Awards from "./containers/Laurels/Laurels";
+import Intro from "./containers/Intro/Intro";
 
 //style css
 import "./app.css";
+import Gallery from "./containers/Gallery/Gallery";
+import FindUs from "./containers/FindUs/FindUs";
+import Subscribe from "./containers/Subscribe/Subscribe";
+import Footer from "./containers/Footer/Footer";
 function App() {
   return (
     <>
@@ -15,6 +21,12 @@ function App() {
       <AboutUs />
       <SpecialMenu />
       <Chef />
+      <Intro/>
+      <Awards/>
+      <Gallery/>
+      <FindUs/>
+      <Subscribe/>
+      <Footer/>
     </>
   );
 }
